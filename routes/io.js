5e8dae6bcae6a687efbdc4d2ac7e2952ff113eec
anantlk <anant.lk.1997@gmail.com>
2018-03-29 21:23:09 +0530

    update:

--- routes/io.js
+++ routes/io.js
@@ -1,4 +1,10 @@
 var count=0;
+// var url="http://api.msg91.com/api/sendhttp.php?sender=MSGIND&route=4&mobiles=9831900563&authkey=206480ABKZqz9f5DBl5abbf43a&country=91&message=Hello! This is a test message";
+var http=require('http');
+
+var TMClient = require('textmagic-rest-client');
+  
+var c = new TMClient('Anant', 'C7XDKZOQZo6HvhJwtUw0MBcslfqwtp4');
 module.exports = function(io){
 	
 	io.on("connection",function(socket){
@@ -14,7 +20,23 @@ module.exports = function(io){
 
 		socket.on("message",function(msg){
 			console.log("Message:"+msg);
-			io.emit("message",msg);
+			c.Messages.send({text: 'test message', phones:'9831900563'}, function(err, res){
+			    console.log('Messages.send()', err, res);
+			});
+
+
+			if(msg.toLowerCase()=="light on")
+			{
+				io.emit("message","Switching on the lights");
+			}
+			if(msg.toLowerCase()=="light off")
+			{
+				io.emit("message","Switching off the lights");
+			}
+			if(msg.toLowerCase()=="number of people in room?")
+			{
+				io.emit("message","Count");
+			}
 		});
 
 		
