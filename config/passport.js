e5dc70ea2e45e6b0febe85ef75b9ffeefa3673e2
Anant Kakrania <anant.lk.1997@gmail.com>
2018-05-21 17:49:30 +0530

    clean the code

--- config/passport.js
+++ config/passport.js
@@ -6,65 +6,49 @@ module.exports = (passport) => {
         done(null, user.id);
     });
     passport.deserializeUser((id, done) => {
-        User.findById(id).exec()
-        .then((err,user) => {
+        User.findById(id, (err, user) => {
             done(err, user);
-        })
-        .catch(done);
+        });
     });
 
 
 
     passport.use('newRegister', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
         usernameField : 'email',
         passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
+        passReqToCallback : true
     },
     (req, email, password, done) => {
         process.nextTick(() => {
-
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'email' :  email }).exec().
-        .then((user) => {
-            // check to see if theres already a user with that email
+        User.findOne({ 'email' :  email }, (err, user) => {
+            if (err)
+                return done(err);
             if (user) {
                 return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-            } 
-            else {
-
-                // if there is no user with that email
-                // create the user
+            } else {
                 var newUser = new User();
-
-                // set the user's local credentials
                 newUser.username = req.body.username;
                 newUser.email = email;
                 newUser.password = newUser.generateHash(password);
                 newUser.phone = req.body.phone;
-                // save the user
-                newUser.save()
-                .catch((err) => {
+                newUser.save((err) => {
                     if (err)
                         throw err;
                     return done(null, newUser);
                 });
             }
 
-        })
-        .catch(done);    
+        });    
 
         });
 
     }));
      passport.use('login', new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
             usernameField : 'email',
             passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+            passReqToCallback : true 
         },
-        (req, email, password, done) => { // callback with email and password from our form
+        (req, email, password, done) => {
             User.findOne({ 'email' :  email }, (err, user) => {
                 if (err)
                     return done(err);
